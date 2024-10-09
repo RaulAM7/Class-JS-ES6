475f97fb97714fe978b61e1381d3ac555a1a83ab
RaulAM <raulartilesm@gmail.com>
2024-10-09 12:03:45 +0100

    Update in the case that the pc could possibly run down again

---
+++ main.js
@@ -0,0 +1,15 @@
+// Arrow function 
+
+function addTwo (a,b) {
+    return a+b
+}
+
+const addTwoNums = (a,b) => {
+    return a + b
+}
+
+const addTwoNumbers = (a, b) => (a+b)
+
+console.log(addTwo(2,2))
+console.log(addTwoNums(2, 2))
+console.log(addTwoNumbers(2, 2))
