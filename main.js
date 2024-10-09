6ecacbfe474e2842c1ae47c9f9c32f5d7891a20d
RaulAM <raulartilesm@gmail.com>
2024-10-09 12:21:41 +0100

    Update: procesada 50% de la clase

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ const addTwoNumbers = (a, b) => (a+b)
 console.log(addTwo(2,2))
 console.log(addTwoNums(2, 2))
 console.log(addTwoNumbers(2, 2))
+
+
+// Funcion constructora 
+
+function Nums () {
+    this.data = [1, 2, 3]
+
+    this.printNumsCallback = function () {
+        this.data.forEach((element, index) => {
+            console.log(element)
+        })
+    }
+}
+let nums = new Nums()
+nums.printNumsCallback()
+
+
+// METODOS DE STRINGS
